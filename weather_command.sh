b6d315d19531617761ce5417d129f3c1c61423c7
Syed Hashir Ayub <100124377+Syedhashirayub@users.noreply.github.com>
2023-07-13 14:50:43 +0530

    Add files via upload

---
+++ weather_command.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install python3-pip
+sudo apt install python3.10-venv
+python3 -m venv airflow_venv     #creating virtual environment
+source airflow_venv/bin/activate
+sudo pip install pandas
+sudo pip install s3fs
+sudo pip install apache-airflow      
+airflow standalone               #If facing any error downgrade from pydantic 2.0.2 to 1.10.10
+
+#for getting session token
+sudo apt  install awscli
+aws configure
+aws sts get-session-token
